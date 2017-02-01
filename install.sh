ffd703d6664a997562744d99ecbeadfd7f41b462
Ali Hakkı Demiral <alihakkidemiral@gmail.com>
2017-02-01 18:23:17 +0300

    added select_keymap function

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+select_keymap(){
+    keymap_list=(us trq)
+    select KEYMAP in "${keymap_list[@]}"; do
+        if contains_element "$KEYMAP" ${keymap_list[@]}; then
+            loadkeys $KEYMAP
+            echo "$KEYMAP keypad has been set."
+            break
+        else
+            echo "Invalid option"
+        fi
+    done
+}
+
+
+  contains_element() {
+    for e in "${@:2}"; do [[ $e == $1 ]] && break; done;
+}
+
+select_keymap
